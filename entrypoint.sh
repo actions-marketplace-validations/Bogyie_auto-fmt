927248a3b448e888713bae4ff5abda71bfd0fb6d
bogyeong <bogyeong@lguplus.co.kr>
2023-05-14 15:49:47 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 # INPUT_BASE_REF
 # INPUT_HEAD_REF 
 # INPUT_TRIGGERING_ACTOR
-PULL_NUMBER=$(grep -o '[0-9]*' $INPUT_REF)
+PULL_NUMBER=$(echo "$INPUT_REF" | grep -o '[0-9]*')
 
 git clone "https://$INPUT_TRIGGERING_ACTOR:$INPUT_TOKEN@github.com/$INPUT_REPOSITORY.git" -b $INPUT_BASE_REF repo
 
@@ -14,16 +14,18 @@ if [ -d repo ]; then
     cd repo
     git checkout -t origin/$INPUT_HEAD_REF
     
-    edited_files=`git diff --name-only origin/$INPUT_BASE_REF origin/$INPUT_HEAD_REF | grep -i '.tf$' `
+    edited_files=$(git diff --name-only origin/$INPUT_BASE_REF origin/$INPUT_HEAD_REF | grep -i '.tf$')
+    echo $edited_files
+
     reformated_files=""
     for edited_file in $edited_files; do
-        reformated_file=$(terraform fmt -list=true $edited_file)
+        reformated_file=$(terraform fmt -list=true "$edited_file")
         if [ -n "$reformated_file" ]; then
-            reformated_files+="- $reformated_file\n"
+            echo $reformated_files
+            reformated_files="$reformated_files\n-$reformated_file"
         fi
     done
-
-    git add .
+    echo $reformated_files
 
     # If exist re-formatting .tf file
     if [ -n "$reformated_files"]; then
@@ -31,7 +33,9 @@ if [ -d repo ]; then
         git config --global user.name $INPUT_TRIGGERING_ACTOR && \
             git config --global user.email $INPUT_TRIGGERING_ACTOR@github.com && \
             git commit --amend --no-edit && \
+            git add . && \
             git push --force && \
+            echo "Req URL: https://api.github.com/repos/$INPUT_REPOSITORY/issues/$PULL_NUMBER/comments" && \
             curl -L \
                 -X POST \
                 -H "Accept: application/vnd.github+json" \
