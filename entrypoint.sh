1f26e2a9857f44f37c0cb4b7e055623700bf1700
bogyeong <bogyeong@lguplus.co.kr>
2023-05-14 14:24:18 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,10 @@ if [ -d repo ]; then
     git checkout $INPUT_HEAD_REF
     
     edited_files=`git diff --name-only $INPUT_BASE_REF | grep -i '.tf$' `
-    reformated_files="Next files are reformatted\n\n"
+    reformated_files=""
     for edited_file in $edited_files; do
-        reformated_file=$(terraform fmt $edited_file)
-        if [ -z "$reformated_file" ]; then
+        reformated_file=$(terraform fmt -list=true $edited_file)
+        if [ -n "$reformated_file" ]; then
             reformated_files+="- $reformated_file\n"
         fi
     done
@@ -28,7 +28,7 @@ if [ -d repo ]; then
     git add .
 
     # If exist re-formatting .tf file
-    if [ -z "$(git status)"]; then
+    if [ -n "$reformated_files"]; then
 
         git config --global user.name $INPUT_TRIGGERING_ACTOR && \
             git config --global user.email $INPUT_TRIGGERING_ACTOR@github.com && \
@@ -40,6 +40,6 @@ if [ -d repo ]; then
                 -H "Authorization: Bearer $INPUT_TOKEN"\
                 -H "X-GitHub-Api-Version: 2022-11-28" \
                 https://api.github.com/repos/$INPUT_REPOSITORY/issues/$PULL_NUMBER/comments \
-                -d '{"body":"'$reformated_files'"}'
+                -d '{"body":"Next files are reformatted\n\n'$reformated_files'"}'
     fi
 fi
